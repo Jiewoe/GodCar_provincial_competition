fda2fe393f63116afbc80e9a7c1b308ede3f9f94
Su_yu <2293429724@qq.com>
2022-11-07 23:20:33 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is ahead of 'origin/master' by 2 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   USER/Inc/Display.h
    	modified:   USER/Src/Display.c

---
+++ USER/Inc/Display.h
@@ -0,0 +1,7 @@
+#ifndef __DISPLAY_H__
+#define __DISPLAY_H__
+
+#include "stm32h7xx_hal.h"
+
+
+#endif
--- USER/Src/Display.c
+++ USER/Src/Display.c
@@ -1,2 +1,13 @@
 #include "Display.h"
 
+// dma配置与空闲中断在主函数里面
+
+void DisPlay_Porcess(uint8_t *buffer, uint8_t size)
+{
+    if (buffer[0]!=0xEE)
+    {
+        return;
+    }
+    if ()
+
+}
